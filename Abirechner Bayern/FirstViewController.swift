b24f1e175ab8fe59085be88e075bfb16665eb14f
Jonas Andersson <ja-mail@myway.de>
2018-09-17 15:06:04 +0200

    Initial Commit

---
+++ Abirechner Bayern/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Abirechner Bayern
+//
+//  Created by Jonas Andersson on 17.09.18.
+//  Copyright © 2018 bambuzleapps. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
